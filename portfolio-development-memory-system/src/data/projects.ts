355c78872091cd55c7cb52bb60c6a1d6dd71aaf8
NEK360 <nikitabaranov1789@gmail.com>
2026-08-04 04:28:55 +0300

    Create projects.ts

---
+++ portfolio-development-memory-system/src/data/projects.ts
@@ -0,0 +1,19 @@
+import wbSellerDashboard from "./wb-seller-dashboard.jpg";
+import wbProductCard1 from "./wb-product-card-1.jpg";
+import wbProductCard2 from "./wb-product-card-2.jpg";
+import wbSalesAnalytics from "./wb-sales-analytics.jpg";
+
+import gasProjectFiles from "./gas-project-files.jpg";
+import gasSyncCode from "./gas-sync-code.jpg";
+import gasLogSuccess from "./gas-log-success.jpg";
+import gasLogHistory from "./gas-log-history.jpg";
+
+import yandexSettings from "./yandex-direct-settings.jpg";
+import yandexStatistics from "./yandex-direct-statistics.jpg";
+import yandexReport from "./yandex-direct-report.jpg";
+
+import telegramBot from "./github-telegram-bot.jpg";
+
+import dubaiPdf from "./dubai-chocolate-landing.pdf";
+import kazachyPdf from "./kazachy-hleb-analysis.pdf";
+import tildaPdf from "./vb-store-tilda-site.pdf";
